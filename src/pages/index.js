30e47dde4a74f6a939a990eea06800b833f2e741
Brayan233 <brayan.oduro@gmail.com>
2022-08-16 05:58:48 +0200

    vglnk xero

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,29 +8,12 @@ import * as styles from "../components/index.module.css"
 
 const links = [
   {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial",
+    text: "AFFILIATION",
+    url: "https://redirect.viglink.com?u=http%3A%2F%2Fxero.com&key=eb65c5cf937ca20a7e018b363814b881",
     description:
       "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
   },
-  {
-    text: "Examples",
-    url: "https://github.com/gatsbyjs/gatsby/tree/master/examples",
-    description:
-      "A collection of websites ranging from very basic to complex/complete that illustrate how to accomplish specific tasks within your Gatsby sites.",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Learn how to add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-  },
+
 ]
 
 const samplePageLinks = [
